2f36b34ef358af3fe92c52ebbeef3c2bf6444ebb
Edwin Zhou <edwinzzhou@gmail.com>
2018-05-09 02:12:55 -0400

    Remove redundant props made App stateless

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { firebase } from './firebase';
 
 import Create from './components/Create';
 import Navbar from './components/Navbar';
@@ -12,39 +11,18 @@ import Home from './components/Home';
 import * as routes from './constants/routes';
 import withAuthentication from './components/withAuthentication';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      competitions: [],
-    };
-
-    this.addToCompetitions = this.addToCompetitions.bind(this);
-  }
-  addToCompetitions(competition) {
-    const competitions = this.state.competitions.concat(competition);
-    this.setState({
-      competitions: competitions
-    });
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <div className="container">
-            <Route exact path={routes.HOME} render={() => <Home />} />
-            <Route path={routes.CREATE_COMP} render={() => <Create />} />
-            <Route path={routes.COMPETITIONS} render={() => <CompetitionsPage competitions={this.state.competitions} />} />
-            <Route path={routes.LOG_IN} render={() => <LogIn />} />
-            <Route path={routes.SIGN_UP} render={() => <SignUp />} />
-          </div>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () =>
+  <BrowserRouter>
+    <div>
+      <Navbar />
+      <div className="container">
+        <Route exact path={routes.HOME} render={() => <Home />} />
+        <Route path={routes.CREATE_COMP} render={() => <Create />} />
+        <Route path={routes.COMPETITIONS} render={() => <CompetitionsPage />} />
+        <Route path={routes.LOG_IN} render={() => <LogIn />} />
+        <Route path={routes.SIGN_UP} render={() => <SignUp />} />
+      </div>
+    </div>
+  </BrowserRouter>
 
 export default withAuthentication(App);
