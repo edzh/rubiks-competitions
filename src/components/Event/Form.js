52ed6efee21f49f6d067da73f6d533a2fe4d9a42
Edwin Zhou <edwinzzhou@gmail.com>
2018-07-23 17:55:33 -0400

    Fix forms

--- src/components/Event/Form.js
+++ src/components/Event/Form.js
@@ -12,6 +12,7 @@ class EventForm extends Component {
     this.state = {
       compid: this.props.compid,
       name: '',
+      round: '',
       startTime: '',
       endTime: '',
       date: this.props.date,
@@ -21,10 +22,10 @@ class EventForm extends Component {
   }
 
   onSubmit(event) {
-    const { name, startTime, endTime, date } = this.state;
+    const { name, round, startTime, endTime, date } = this.state;
 
     event.preventDefault();
-    db.doCreateEvent(this.props.compid, name, startTime, endTime, date);
+    db.doCreateEvent(this.props.compid, name, round, startTime, endTime, date);
     this.setState({
       name: '',
       startTime: '',
@@ -43,12 +44,32 @@ class EventForm extends Component {
     return (
       <div>
         <form onSubmit={this.onSubmit}>
-          <input
-            value={name}
-            onChange={event => this.setState(byPropKey('name', event.target.value))}
-            type="text"
-            className="form-control"
-          />
+          <select className="form-control" onChange={event => this.setState(byPropKey('name', event.target.value))}>
+            <option value="">---</option>
+            <option value="Rubik's Cube">Rubik's Cube</option>
+            <option value="4x4x4 Cube">4x4x4 Cube</option>
+            <option value="5x5x5 Cube">5x5x5 Cube</option>
+            <option value="6x6x6 Cube">6x6x6 Cube</option>
+            <option value="7x7x7 Cube">7x7x7 Cube</option>
+            <option value="3x3x3 Blindfolded">3x3x3 Blindfolded</option>
+            <option value="3x3x3 Fewest Moves">3x3x3 Fewest Moves</option>
+            <option value="3x3x3 One Handed">3x3x3 One Handed</option>
+            <option value="3x3x3 With Feet">3x3x3 With Feet</option>
+            <option value="Megaminx">Megaminx</option>
+            <option value="Pyraminx">Pyraminx</option>
+            <option value="Rubik's Clock">Rubik's Clock</option>
+            <option value="Square-1">Square-1</option>
+            <option value="4x4x4 Blindfolded">4x4x4 Blindfolded</option>
+            <option value="5x5x5 Blindfolded">5x5x5 Blindfolded</option>
+            <option value="3x3x3 Multi-Blind">3x3x3 Multi-Blind</option>
+          </select>
+          <select className="form-control" onChange={event => this.setState(byPropKey('round', event.target.value))}>
+            <option value="">---</option>
+            <option value="Round 1">Round 1</option>
+            <option value="Round 2">Round 2</option>
+            <option value="Final">Final</option>
+            <option value="Combined Final">Combined Final</option>
+          </select>
           <input
             value={startTime}
             onChange={event => this.setState(byPropKey('startTime', event.target.value))}
